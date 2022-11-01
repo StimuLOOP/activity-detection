dcaed98a7103586fa9a11210220e17b525855bef
Alain Ryser <aryser@inf.ethz.ch>
2022-11-01 09:22:21 +0100

    Added some changes to help load faulty sensor measurements

--- utils/utils.py
+++ utils/utils.py
@@ -1,5 +1,9 @@
 import ntpath
 import datetime
+import pandas as pd
+from utils.constants import *
+from joblib import load
+import numpy as np
 
 def path_leaf(path):
     head, tail = ntpath.split(path)
@@ -9,3 +13,20 @@ def log(msg):
     with open('log.txt', 'a') as fp:
         timestamp =  datetime.datetime.now()
         fp.write(f"[{str(timestamp)}] {msg}\n")
+
+def load_csv(fn):
+    # Assemble dataset of the form (num_samples, num_measurements, w_size)
+    df = pd.read_csv(fn,on_bad_lines='skip')
+    for key in df.keys():
+        if not 'prediction' in key:
+            df[key] = pd.to_numeric(df[key],errors='coerce')
+    df = df.dropna()
+    return df
+
+def make_predictions(model_fn, data, task, w_size=128):
+    model = load(model_fn)
+    means = np.repeat(model[0].mean_.reshape(1,-1), data.shape[0], axis=0)
+    data[data==np.inf] = means[data==np.inf]
+    curr_preds = model.predict(data)
+    curr_preds = np.array([PRED_TO_STRING[task][label] for label in curr_preds]).repeat(w_size)
+    return curr_preds
